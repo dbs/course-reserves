620812c836f78302584b5dea66ff140b02d5f203
Victory000 <victoria.a.lacroix@gmail.com>
2015-05-21 14:18:13 -0400

    Added various files for the Flask application.
    course-reserves.py/fcgi to run the application.
    database.py to hold all of our db methods
    babel.cfg for internationalization
    config.ini as a config file for application settings
    schema.sql for the table
    
    Signed-off-by: Victory000 <victoria.a.lacroix@gmail.com>

---
+++ reserves/database.py
@@ -0,0 +1,3 @@
+import psycopg2
+
+#This will hold all of our DB methods.
---
+++ reserves/schema.sql
@@ -0,0 +1,4 @@
+-- SQL Schema
+
+-- Table Definitions
+CREATE TABLE reserve (id INTEGER PRIMARY KEY, course_code TEXT, instructor TEXT, bookbag_id INTEGER);
