b26202c23cac1a1dadbbf05c24af6cc150213b62
Victory000 <victoria.a.lacroix@gmail.com>
2015-05-25 09:21:07 -0400

    Added ability to add/remove reserves.
    Addition, modification, and deletion of reserves are all present.
    course_reserves.py handles form parsing.
    database.py handles database interactions.
    adminform.html is adapted from what existed before.
    Fixed display of data when switching languages.
    
    Signed-off-by: Victory000 <victoria.a.lacroix@gmail.com>

--- reserves/course_reserves.py
+++ reserves/course_reserves.py
@@ -32,6 +32,10 @@ cmd_opt, junk = parser.parse_args()
 opt['DEBUG']  = cmd_opt.DEBUG
 opt['VERBOSE'] = cmd_opt.VERBOSE
 
+if opt['VERBOSE']:
+    print('Using options:')
+    print(opt)
+
 @babel.localeselector
 def select_locale():
     "Selects the locale. Babel uses this to determine which language to go with."
@@ -54,7 +58,58 @@ def lang_switch(lang):
         if opt['VERBOSE']:
             print('Language switched to: ' + lang)
         session['LANG'] = lang
-        return render_template('root.html', opt=opt), 200
+        return render_template('root.html', opt=opt, data=database.get_reserves()), 200
+
+@app.route(opt['APP_ROOT']+'admin/', methods=['GET', 'POST'])
+def admin():
+    return render_template('adminform.html', opt=opt), 200
+
+@app.route(opt['APP_ROOT']+'add/', methods=['POST'])
+def add_reserve():
+    try:
+        form = request.form
+        code = form['course_code']
+        instructor = form['instructor']
+        bookbag = form['bookbag_id']
+        database.add_reserve(code, instructor, bookbag)
+    except Exception, ex:
+        print('Error occurred while parsing addition: ')
+        print(ex)
+        message = 'Couldn\'t submit form.'
+        return render_template('adminform.html', opt=opt, message=message), 200
+    message = 'Form successfully submitted.'
+    return render_template('adminform.html', opt=opt, message=message), 200
+
+@app.route(opt['APP_ROOT']+'edit/', methods=['POST'])
+def edit_reserve():
+    try:
+        form = request.form
+        id = form['reserve_id']
+        code = form['course_code']
+        instructor = form['instructor']
+        bookbag = form['bookbag_id']
+        database.edit_reserve(id, code, instructor, bookbag)
+    except Exception, ex:
+        print('Error occurred while parsing edit: ')
+        print(ex)
+        message = 'Couldn\'t submit form.'
+        return render_template('admin', opt=opt, message=message), 200
+    message = 'Form successfully submitted.'
+    return render_template('admin', opt=opt, message=message), 200
+
+@app.route(opt['APP_ROOT']+'delete/', methods=['POST'])
+def delete_reserve():
+    try:
+        form = request.form
+        id = form['reserve_id']
+        database.delete_reserve(id)
+    except Exception, ex:
+        print('Error occurred while parsing deletion: ')
+        print(ex)
+        message = 'Couldn\'t submit form.'
+        return render_template('adminform.html', opt=opt, message=message), 200
+    message = 'Form successfully submitted.'
+    return render_template('adminform.html', opt=opt, message=message), 200
     
 app.secret_key = opt['SECRET']
 
--- reserves/database.py
+++ reserves/database.py
@@ -1,4 +1,5 @@
 import psycopg2
+import random
 from course_reserves import opt
 
 def get_db():
@@ -19,6 +20,7 @@ def get_db():
             print(ex)
 
 def get_reserves():
+    """Returns all of the existing reserves."""
     dbh = get_db()
     cur = dbh.cursor()
     try:
@@ -31,8 +33,58 @@ def get_reserves():
         return result
     except Exception, ex:
         if opt['VERBOSE']:
-            print('Couldn\'t get table for reserves.')
+            print('Couldn\'t get table for reserves: ')
+            print(ex)
     dbh.close()
     return None
 
-#This will hold all of our DB methods.
+def add_reserve(code, instructor, bookbag):
+    """Adds a reserve."""
+    dbh = get_db()
+    cur = dbh.cursor()
+    try:
+        cur.execute("""INSERT INTO reserve VALUES (%s, %s, %s, %s)""", 
+                    (random.SystemRandom().randint(-0xFFFFFF, 0xFFFFFF),
+                    code, instructor, bookbag))
+    except Exception, ex:
+        if opt['VERBOSE']:
+            print('Couldn\'t add a reserve: ')
+            print(ex)
+        raise ex
+    dbh.commit()
+    dbh.close()
+
+def edit_reserve(id, code, instructor, bookbag):
+    """
+    Edits an existing reserve.
+    """
+    dbh = get_db()
+    cur = dbh.cursor()
+    try:
+        cur.execute("""UPDATE TABLE reserve SET course_code, instructor,
+            bookbag_id = %s, %s, %s WHERE id = %s""", (code, instructor, bookbag, id))
+    except Exception, ex:
+        if opt['VERBOSE']:
+            print('Couldn\'t edit a reserve: ')
+            print(ex)
+        raise ex
+    dbh.commit()
+    dbh.close()
+
+def delete_reserve(id):
+    """
+    Deletes a reserve.
+
+    Careful!
+    """
+    dbh = get_db()
+    cur = dbh.cursor()
+    try:
+        cur.execute("""DELETE FROM reserve where id = %s""" % id)
+    except Exception, ex:
+        if opt['VERBOSE']:
+            print('Could not delete reserve:')
+            print(ex)
+        raise ex
+    dbh.commit()
+    dbh.close()
