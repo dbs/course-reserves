a68fbc3de864fec47e2747045362a4f3e6509649
Victory000 <victoria.a.lacroix@gmail.com>
2015-05-25 09:17:55 -0400

    Added language switching.
    Language switching, in this case, uses session variables.
    
    Signed-off-by: Victory000 <victoria.a.lacroix@gmail.com>

--- reserves/course_reserves.py
+++ reserves/course_reserves.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from flask import Flask, abort, request, render_template, \
-                    make_response, g
+from flask import Flask, abort, request, redirect, url_for, \
+                    render_template, make_response, g, session
 from flask_login import LoginManager, login_required, current_user, \
                     login_user, logout_user
 from flask_babelex import Babel
@@ -11,8 +11,8 @@ from conf import ConfigFile
 import sys
 import ldap
 import psycopg2
-import database
 import StringIO
+import database
 
 app = Flask(__name__)
 app.root_path = abspath(dirname(__file__))
@@ -32,9 +32,31 @@ cmd_opt, junk = parser.parse_args()
 opt['DEBUG']  = cmd_opt.DEBUG
 opt['VERBOSE'] = cmd_opt.VERBOSE
 
-@app.route(opt['APP_ROOT'], methods=['GET','POST'])
-def visit_root():
-    return render_template('blank.html', data=database.get_reserves()), 200
+@babel.localeselector
+def select_locale():
+    "Selects the locale. Babel uses this to determine which language to go with."
+    try:
+        if opt['VERBOSE']:
+            print(session['LANG'])
+        return session['LANG']
+    except Exception, ex:
+        return opt['LANG']
+
+@app.route(opt['APP_ROOT'], methods=['GET', 'POST'])
+@app.route(opt['APP_ROOT']+'view/', methods=['GET', 'POST'])
+def view_reserves():
+    return render_template('root.html',
+            data=database.get_reserves(), opt=opt), 200
+
+@app.route(opt['APP_ROOT']+'lang/<lang>/', methods=['GET', 'POST'])
+def lang_switch(lang):
+    if lang in ['en', 'fr']:
+        if opt['VERBOSE']:
+            print('Language switched to: ' + lang)
+        session['LANG'] = lang
+        return render_template('root.html', opt=opt), 200
+    
+app.secret_key = opt['SECRET']
 
 if __name__ == '__main__':
     app.run(debug=opt['DEBUG'], host=opt['HOST'], port=opt['PORT'])
