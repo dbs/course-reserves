e4bc6bd7b1c0cf5f1ed707ae8d176d1077a21b8f
Dan Scott <dan@coffeecode.net>
2019-09-05 10:55:17 -0400

    Add app_root config parameter back in the mix
    Turns out this was necessary to get things working at a relocatable
    location. At least for now.
    
    Signed-off-by: Dan Scott <dan@coffeecode.net>

--- reserves/course_reserves.py
+++ reserves/course_reserves.py
@@ -12,6 +12,36 @@ import ldap
 import psycopg2
 import traceback
 
+class LocalCGIRootFix(object):
+    """Wrap the application in this middleware if you are using FastCGI or CGI
+    and you have problems with your app root being set to the cgi script's path
+    instead of the path users are going to visit
+    .. versionchanged:: 0.9
+       Added `app_root` parameter and renamed from `LighttpdCGIRootFix`.
+    :param app: the WSGI application
+    :param app_root: Defaulting to ``'/'``, you can set this to something else
+        if your app is mounted somewhere else.
+
+    Clone of workzeug.contrib.fixers.CGIRootFix, but doesn't strip leading '/'
+    """
+
+    def __init__(self, app, app_root='/'):
+        self.app = app
+        self.app_root = app_root
+
+    def __call__(self, environ, start_response):
+        # only set PATH_INFO for older versions of Lighty or if no
+        # server software is provided.  That's because the test was
+        # added in newer Werkzeug versions and we don't want to break
+        # people's code if they are using this fixer in a test that
+        # does not set the SERVER_SOFTWARE key.
+        if 'SERVER_SOFTWARE' not in environ or \
+           environ['SERVER_SOFTWARE'] < 'lighttpd/1.4.28':
+            environ['PATH_INFO'] = environ.get('SCRIPT_NAME', '') + \
+                environ.get('PATH_INFO', '')
+        environ['SCRIPT_NAME'] = self.app_root.rstrip('/')
+        return self.app(environ, start_response)
+
 app = flask.Flask(__name__, static_url_path='/static', static_folder='static')
 app.root_path = abspath(dirname(__file__))
 
@@ -41,6 +71,7 @@ if opt['VERBOSE']:
     print('Using options:')
     print(opt)
 
+app.wsgi_app = LocalCGIRootFix(app.wsgi_app, app_root=opt['APP_ROOT'])
 babel = Babel(app)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
